5e5af573332d2d89a4133cde34afd8f63af2437f
leaez@163.com <leaez@163.com>
2020-04-22 22:20:29 +0800

    test mod

---
+++ modulex/bar.py
@@ -0,0 +1,8 @@
+
+#!/usr/bin/python3
+# Filename: support.py
+ 
+def print_func( par ):
+    print ("Hello : ", par)
+    return
+
---
+++ modulex/foo/mod2.py
@@ -0,0 +1,7 @@
+
+
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+ 
+def print_mod2():
+   print ("I'm in mod2")
